d48efe3427b3734b3a3d2f1a937b58ada46172c7
James Page <jp00965@surrey.ac.uk>
2019-11-19 19:08:19 +0000

    Added contact form

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -10,4 +10,31 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     assert_select 'p', 'Copyright James Page 2019'
   end
 
+  test "should get contact" do
+    get contact_url
+    assert_response :success
+
+    assert_template layout: 'application'
+
+    assert_select 'title', 'CricketLeagues.com'
+    assert_select 'h1', 'Contact Us'
+    assert_select 'p', 'Complete the following form to email us.'
+  end
+
+  test "should post request contact but no email" do
+    post request_contact_url
+
+    assert_response :redirect
+    assert_not_empty flash[:alert]
+    assert_nil flash[:notice]
+  end
+
+  test "should post request contact" do
+    post request_contact_url, params: {name: "Someone", email: "someone@example.com", telephone: "01234567890", message: "DUMMY"}
+
+    assert_response :redirect
+    assert_nil flash[:alert]
+    assert_not_empty flash[:notice]
+  end
+
 end
