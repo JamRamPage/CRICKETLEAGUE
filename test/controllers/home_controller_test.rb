f23cfd8d2cfc358ee8a781a4322f595d5e73cfe7
James Page <jp00965@surrey.ac.uk>
2019-11-19 18:52:25 +0000

    Added testing to home page

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,8 +2,12 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get home_home_url
+    get root_url
     assert_response :success
+
+    assert_select 'title', 'CricketLeagues.com'
+    assert_select 'h1', 'CricketLeagues.com'
+    assert_select 'p', 'Copyright James Page 2019'
   end
 
 end
